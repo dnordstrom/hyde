709a2f6a4dc6dd3efc720d4ed6b2404a028fe373
Daniel Nordstrom <d@nintera.com>
2011-12-15 06:31:25 +0100

    Commenting.

--- lib/hyde/helpers/path_helper.rb
+++ lib/hyde/helpers/path_helper.rb
@@ -1,5 +1,17 @@
 module Hyde
   module PathHelper
+    # Sets up path to be used by helper (ghost) methods. Path
+    # must have leading slash for helpers to work properly.
+    #
+    # Example:
+    #   use_path("/my_site/_posts")
+    #    => ["my_site", "_posts"]
+    #   current_site
+    #    => "my_site"
+    #   current_dir
+    #    => "_posts"
+    #   current_file
+    #    => false
     def use_path(path)
       # Specify indexes of path fragments, e.g. site
       # identifier is in first part of URL ( /:site/... )
@@ -12,12 +24,14 @@ module Hyde
     end
 
     # Return path fragment at specified index, if available,
-    # otherwise returns false.
+    # otherwise returns false. Used by ghost methods.
     def path_part(part)
       (@path.nil? || @path[part].nil?) ? false : @path[part]
     end
     
-    # Defines ghost methods current_(site|dir|file).
+    # Defines ghost methods current_(site|dir|file|notice) based
+    # on path provided by <code>Hyde::PathHelper::use_path</code>
+    # and parts defined in <code>@parts</code> instance variable.
     def method_missing(method, *args)
       if method =~ /current_(.*)/
         return path_part( @parts[$1.to_sym] ) if @parts.has_key?($1.to_sym)
