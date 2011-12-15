f4b45e8f252c64acaf025ec72145fc15ccc754f9
Daniel Nordstrom <d@nintera.com>
2011-12-15 06:36:23 +0100

    Commenting.

--- lib/hyde/helpers/response_helper.rb
+++ lib/hyde/helpers/response_helper.rb
@@ -1,6 +1,7 @@
 module Hyde
   module ResponseHelper
-    # Generates Rack response array with specified body.
+    # Generates a Rack response array with the provided page
+    # body.
     def respond_with(body)
       [
         # HTTP status code.
@@ -16,11 +17,17 @@ module Hyde
 
     # Generates a Rack response array with a 302 redirect header.
     def redirect_to(path, notice = "")
+      # Append notice to URL if provided.
       notice = "/#{notice.to_s}" unless notice === ""
 
       [
+        # HTTP status code.
         302,
+        
+        # Content type and redirect location.
         { "Content-Type" => "text", "Location" => "#{path + notice}" },
+
+        # Response body, not important in this case.
         [ "302 Redirect" ]
       ]
     end
